70d1061e765ce48f5e18b6a5f71c63fc436adbd8
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-09 16:04:51 -0500

    add sale count to dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,10 +4,14 @@ class Admin::DashboardController < ApplicationController
   def show
     @product_count = Product.count
     @category_count = Category.count
+    @sale_active_count = Sale.active.count
+    @sale_count = Sale.count
 
-    @category = {name: 'Category Count:' ,count: @category_count, link: nil}
-    @product = {name: 'Product Count:' ,count: @product_count, link: admin_products_path}
+    @category = {name: 'Category Count:', count: @category_count, link: nil}
+    @product = {name: 'Product Count:', count: @product_count, link: admin_products_path}
+    @total_sales = {name: 'Total Sales:', count: @sale_count, link: admin_sales_path}
+    @active_sales = {name: 'Active Sales:', count: @sale_active_count, link: admin_sales_path}
 
-    @store_summary = [@product, @category]
+    @store_summary = [@product, @category, @total_sales, @active_sales]
   end
 end
