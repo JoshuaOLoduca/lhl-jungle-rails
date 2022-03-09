db0dc5db61114ceb2c20c2909729290de907d265
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-09 12:51:15 -0500

    add proper defs to admin/category metheds

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,18 +2,18 @@ class Admin::CategoriesController < ApplicationController
     http_basic_authenticate_with name: ENV['ADMIN_LOGIN'], password: ENV['ADMIN_PASSWORD']
 
   def index
-    @products = Product.order(id: :desc).all
+    @categories = Category.order(id: :desc).all
   end
 
   def new
-    @product = Product.new
+    @category = Category.new
   end
 
   def create
-    @product = Product.new(product_params)
+    @category = Category.new(category_params)
 
-    if @product.save
-      redirect_to [:admin, :products], notice: 'Product created!'
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
     else
       render :new
     end
@@ -27,15 +27,8 @@ class Admin::CategoriesController < ApplicationController
 
   private
 
-  def product_params
-    params.require(:product).permit(
-      :name,
-      :description,
-      :category_id,
-      :quantity,
-      :image,
-      :price
-    )
+  def category_params
+    params.require(:product).permit(:name)
   end
 
 end
