291a2f4727dc4e2c69be7db86f54628ed2238b06
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-09 16:16:56 -0500

    add validation to sale

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -2,6 +2,11 @@ class Sale < ActiveRecord::Base
 
   validate :end_date_after_start_date?
 
+  validates :name, presence: true
+  validates :starts_on, presence: true
+  validates :ends_on, presence: true
+  validates :percent_off, presence: true
+
   def self.active
     now = Date.current
     where("ends_on >= ? and starts_on <= ?", now, now)
