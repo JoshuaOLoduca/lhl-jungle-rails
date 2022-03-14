6489a3bbce49e3627d3c3c46815e0cc88efe46c7
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-14 16:27:54 -0400

    add tests for adding user to db

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe User, type: :model do
 describe 'Validations' do
     before(:each) do
       @user = User.new
-      @user.name = 'a name-a'
-      @user.email = 'test@test.ca';
-      @user.password = 'password!';
-      @user.password_confirmation = 'password!';
+      @user.name = 'full name'
+      @user.email = 'test@test.ca'
+      @user.password = 'password!'
+      @user.password_confirmation = @user.password
     end
 
     context 'add user to db with no name' do
@@ -16,6 +16,48 @@ describe 'Validations' do
         @user.save
 
         expect(@user.errors.full_messages).to include('Name can\'t be blank')
+      end
+    end
+
+    context 'add user to db with no password' do
+      it 'return error about password' do
+        @user.password = nil
+        @user.save
+
+        expect(@user.errors.full_messages).to include('Password can\'t be blank')
+      end
+    end
+
+    context 'add user to db with no password confirmation' do
+      it 'return error about password confirmation' do
+        @user.password_confirmation = nil
+        @user.save
+
+        expect(@user.errors.full_messages).to include('Password confirmation can\'t be blank')
+      end
+    end
+
+    context 'add user to db with no email' do
+      it 'return error about email' do
+        @user.email = nil
+        @user.save
+
+        expect(@user.errors.full_messages).to include('Email can\'t be blank')
+      end
+    end
+
+    context 'add user to db with an email in already in use' do
+      it 'return error about email not being unique' do
+        @user2 = User.new
+        @user2.name = @user.name
+        @user2.email = @user.email.upcase
+        @user2.password = @user.password
+        @user2.password_confirmation = @user2.password
+        
+        @user.save
+        @user2.save
+
+        expect(@user2.errors.full_messages).to include('Email has already been taken')
       end
     end
 
