893892d9387fc74fdc1c1f241d6e706a6637a5ec
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-16 18:32:39 -0400

    fix html order email

--- spec/mailers/previews/order_mailer_preview.rb
+++ spec/mailers/previews/order_mailer_preview.rb
@@ -6,14 +6,17 @@ class OrderMailerPreview < ActionMailer::Preview
     @user = User.new(name: 'test', email: 'test@test.com', password: 'testy', password_confirmation: 'testy')
 
     @category = Category.new(name: 'TestCat')
-    @product = Product.new(name: 'prod1', description: 'Duck u', image: 'https://i.cbc.ca/1.5963873.1616692705!/fileImage/httpImage/image.jpeg_gen/derivatives/original_780/face-off.jpeg', price_cents: 100, quantity: 2)
+    @product = Product.new(name: 'prod1', description: 'Prod Desc', image: open_asset('apparel3.jpg'), price_cents: 100, quantity: 2)
+
+    @product2 = Product.new(name: 'prod2', description: 'Prod2 Desc', image: open_asset('apparel4.jpg'), price_cents: 100, quantity: 2)
 
     # @order = Order.new!(total_cents: 200,)
     @order = Order.new(
       email: @user.email,
-      total_cents: 200,
+      total_cents: 400,
       stripe_charge_id: 123_123 # returned by stripe
     )
+    @order.id = -1
 
     @order.line_items.new(
       product: @product,
@@ -21,7 +24,21 @@ class OrderMailerPreview < ActionMailer::Preview
       item_price: 100,
       total_price: 200
     )
+
+    @order.line_items.new(
+      product: @product2,
+      quantity: 2,
+      item_price: 100,
+      total_price: 200
+    )
     # default_url_options[:host] = 'localhost:3000'
     OrderMailer.summary @order
+  end
+
+  private
+
+  # Helper functions
+  def open_asset(file_name)
+    File.open(Rails.root.join('db', 'seed_assets', file_name))
   end
 end
