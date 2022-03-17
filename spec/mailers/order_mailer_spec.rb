f205059371894068e226d57aae0b993e0c2c558b
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-16 20:41:28 -0400

    remove pending notif

--- spec/mailers/order_mailer_spec.rb
+++ spec/mailers/order_mailer_spec.rb
@@ -1,5 +1,7 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe OrderMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  puts "add some examples to (or delete) #{__FILE__}"
 end
